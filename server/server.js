dc485f1757123e4e3e67f0a5a3fc29107feec6bf
MarCoboNa07 <bonaventura.marco.2007@gmail.com>
2024-06-14 17:05:08 +0200

    Deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 5000;
+
+app.use(cors());
 
 app.get("/api", (req, res) => {
     res.json({ "users": ["user1", "user2", "user3", "user4", "user5"] });
