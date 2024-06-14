80368f5a7cd56295ca815ed690f94fbd8d3b08fc
MarCoboNa07 <bonaventura.marco.2007@gmail.com>
2024-06-14 17:17:14 +0200

    Deployment

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ function App() {
   const [backendData, setBackendData] = useState([{}]);
 
   useEffect(() => {
-    fetch("https://ecommerce-vscs.onrender.com/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data);
-      }
-    )
+    fetch("https://ecommerce-vscs.onrender.com/api", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => setBackendData(data))
+      .catch(error => console.error('Error:', error));
   }, []);
 
   return (
